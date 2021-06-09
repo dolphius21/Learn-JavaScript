447ff1d61a75716215cb7dac55a81c61387be3a6
dolphius21 <johnrudolphlee@gmail.com>
2021-06-09 18:18:56 +0800

    Done with array reduce section

---
+++ 13_Array_Reduce/02_Sum_grades.js
@@ -0,0 +1,4 @@
+let grades = [10, 5, 15, 20];
+
+// calculate the sum of these grades
+const sum = grades.reduce((total, currValue) => (total += currValue), 0);
---
+++ 13_Array_Reduce/03_Sum_numbers.js
@@ -0,0 +1,10 @@
+/*
+ * @param {number[]} numbers
+ */
+const sumNumbers = (numbers) => {
+  return numbers.reduce((total, currNum) => total + currNum, 0);
+};
+
+// Sample usage - do not modify
+console.log(sumNumbers([10, 20, 30])); // 60
+console.log(sumNumbers([2, 4, 2, 10])); // 18
