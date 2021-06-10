7ab6cb62ff1a5206a276dabfc2e19d2996720347
dolphius21 <johnrudolphlee@gmail.com>
2021-06-10 21:05:41 +0800

    Done with immutability section

---
+++ 25_Immutability/01_Equality_questions.js
@@ -0,0 +1,65 @@
+const question1 = () => {
+  // return the answer of: true === true = true
+  console.log(true === true);
+  return true === true;
+};
+
+const question2 = () => {
+  // return the answer of: false === false = true
+  console.log(false === false);
+  return false === false;
+};
+
+const question3 = () => {
+  // return the answer of: 14 === 14 = true
+  console.log(14 === 14);
+  return 14 === 14;
+};
+
+const question4 = () => {
+  // return the answer of: "25" === 25 = false
+  console.log('25' === 25);
+  return '25' === 25;
+};
+
+const question5 = () => {
+  // return the answer of: "Hello" === "hello" = false
+  console.log('Hello' === 'hello');
+  return 'Hello' === 'hello';
+};
+
+const question6 = () => {
+  // return the answer of: [] === [] = false
+  console.log([] === []);
+  return [] === [];
+};
+
+const question7 = () => {
+  // return the answer of: new Array() === new Array() = false
+  console.log(new Array() === new Array());
+  return new Array() === new Array();
+};
+
+const question8 = () => {
+  // return the answer of: [1, 2, 3] === [1, 2, 3] = false
+  console.log([1, 2, 3] === [1, 2, 3]);
+  return [1, 2, 3] === [1, 2, 3];
+};
+
+const question9 = () => {
+  // return the answer of: {} === {} = false
+  console.log({} === {});
+  return {} === {};
+};
+
+const question10 = () => {
+  // return the answer of: {id: 10} === {id: 10} = false
+  console.log({ id: 10 } === { id: 10 });
+  return { id: 10 } === { id: 10 };
+};
+
+const question11 = () => {
+  // return the answer of: {isDisabled: true} === {isDisabled: true} = false
+  console.log({ isDisabled: true } === { isDisabled: true });
+  return { isDisabled: true } === { isDisabled: true };
+};
