fd583cab87bfd531ecae70b9a1e6077fa24eece6
dolphius21 <johnrudolphlee@gmail.com>
2021-06-11 16:30:25 +0800

    Done with introduction to classes section

---
+++ 27_Introduction_To_Classes/02_Create_person/index.js
@@ -0,0 +1,11 @@
+/*
+ * @param {string} fName
+ * @param {string} lName
+ */
+const createPerson = (fName, lName) => {
+  return new Person(fName, lName);
+};
+
+// Sample usage - do not modify
+console.log(createPerson('Sam', 'Doe'));
+console.log(createPerson('Charlie', 'Green'));
