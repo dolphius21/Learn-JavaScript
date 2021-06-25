860c73f13e8353cc4f16ece9868c1e53130a7058
dolphius21 <johnrudolphlee@gmail.com>
2021-06-25 17:27:32 +0800

    Done with intro to async await section

---
+++ 64_Intro_to_Async_Await/02_Use_async_function.js
@@ -0,0 +1,7 @@
+// do NOT modify the function
+const getMinimumVotingAge = async () => {
+  return 18;
+};
+
+// Sample usage. TODO: write it yourself
+getMinimumVotingAge().then((age) => console.log(age));
---
+++ 64_Intro_to_Async_Await/03_Async_voting.js
@@ -0,0 +1,12 @@
+/*
+ * @param {number} age
+ */
+const canVote = async (age) => age >= 18;
+
+// Sample usage - do not modify
+canVote(20).then((result) => {
+  console.log(result); // true
+});
+canVote(15).then((result) => {
+  console.log(result); // false
+});
